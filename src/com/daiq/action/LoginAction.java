98166eeb23a347a0972fdeed2c5f53ac5596262a
daiqiang <342671940@QQ.COM>
2014-06-11 15:12:11 +0800

    v0.2
    做了一个登陆验证的filter，如果用户之前没有登陆过，则不允许直接访问loginSuccess.jsp

--- src/com/daiq/action/LoginAction.java
+++ src/com/daiq/action/LoginAction.java
@@ -1,6 +1,7 @@
 package com.daiq.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 
@@ -9,16 +10,18 @@ import com.opensymphony.xwork2.ActionSupport;
 public class LoginAction extends ActionSupport{
 	public String execute() {
 		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpSession session = ServletActionContext.getRequest().getSession();
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		System.out.println(username);
 		
 		if("dai".equals(username)&&"123".equals(password)){
+			session.setAttribute("username", username);
+			session.setMaxInactiveInterval(30);
 			return SUCCESS;
 		}else{
 			return "fail";
 		}
-		
 		
 	}
 }
---
+++ src/com/daiq/filter/LoginCheckFilter.java
@@ -0,0 +1,59 @@
+package com.daiq.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckFilter implements Filter{
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+	    HttpServletResponse res = (HttpServletResponse) response;
+	    HttpSession session = req.getSession(true);
+	    
+	    String currentURL = req.getRequestURI();//截取当前文件名用于比较
+	    System.out.println("currentURL="+currentURL);
+	    
+	    if("/ScoreSystem/login.jsp".equals(currentURL)){
+	    	System.out.println("欢迎来到登录页面");
+		    chain.doFilter(req, res);
+	    }else{
+	    	//从session里取的用户名信息
+		    String username = (String) session.getAttribute("username");
+		    
+		    //判断如果没有取到用户信息,就跳转到登陆页面
+		     if (username == null || "".equals(username)) {
+		       //跳转到登陆页面
+		    	 System.out.println("doing Filter");
+		    	 System.out.println(currentURL);
+		       res.sendRedirect("http://"+req.getHeader("Host")+"/ScoreSystem/login.jsp");
+		     }
+		     else {
+		       //已经登陆,继续此次请求
+		       chain.doFilter(req,res);
+		     }
+	    }
+	    
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
